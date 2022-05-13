2b41c9a7405d406d89765a5767e4db41d52a4662
joaocarazzato <joao.carazzato@sou.inteli.edu.br>
2022-05-13 10:07:42 -0300

    Link de todos os sites até o momento.

---
+++ src/Frontend/pagina_de_likes_do_recrutador/pagina_De_Like_Do_Candidato.js
@@ -0,0 +1,40 @@
+var match = document.getElementById('botao1')
+var main = document.getElementById('main')
+var likes = document.getElementById('botao2')
+
+function criarCardLike () {
+
+var div1 = document.createElement('div')
+var div2 = document.createElement('div')
+var p = document.createElement('p')
+div1.className = 'card'
+div2.className = 'card-body'
+p.className = 'card-text'
+
+main.appendChild(div1)
+div1.appendChild(div2)
+div2.appendChild(p)
+p.innerText = 'Maria Luiza Torres deu Match com sua vaga de Analista de Sistemas!'
+
+}
+
+function mudancaDeCorDoBotao (botao, outroBotao) {
+
+    botao.style.backgroundColor = '#530084'
+    botao.style.color = 'white'
+    outroBotao.style.backgroundColor = 'white'
+    outroBotao.style.color = '#530084'
+    outroBotao.style.borderColor = '#530084'
+}
+
+match.addEventListener('click', function () {
+    mudancaDeCorDoBotao(match, likes)
+    criarCard()
+    console.log(this.className)
+})
+
+likes.addEventListener('click', function () {
+    criarCardLike()
+    mudancaDeCorDoBotao(likes, match)
+    console.log(this.className)
+})
