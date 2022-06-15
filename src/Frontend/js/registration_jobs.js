cc228227f0ca8a177a62eb9328afd60de23591d0
melyssa08 <melyssa.rojas@sou.inteli.edu.br>
2022-06-14 23:51:24 -0300

    Modificações para a página de cadastro de vaga

--- src/Frontend/js/registration_jobs.js
+++ src/Frontend/js/registration_jobs.js
@@ -50,31 +50,61 @@ for (let i = 0; i < botao.length; i++) {
 var jobsName = document.getElementById('registrationJobs-name-job');
 var jobsType = document.getElementById('registrationJobs-type');
 var jobsTime = document.getElementById('registrationJobs-time');
-var jobsSalary = document.getElementById('registrationJobs-salary');
 var jobsModality = document.getElementById('registrationJobs-modality');
 var jobsShift = document.getElementById('registrationJobs-shift');
-var jobsEmail = document.getElementById('registrationJobs-email');
+var jobsSalaryMin = document.getElementById('registrationJobs-salary-min');
 var jobsSchooling = document.getElementById('registrationJobs-schooling');
-var jobsNumber = document.getElementById('registrationJobs-number');
+var jobsSalaryMax = document.getElementById('registrationJobs-salary-max');
 var jobsCP = document.getElementById('registrationJobs-cp');
 var jobsProficiency = document.getElementById('registrationJobs-profiency');
 var jobsNameCompany = document.getElementById('registrationJobs-name-company');
 var jobsDescription = document.getElementById('registrationJobs-description-jobs');
-var companyDescription = document.getElementById('registrationJobs-description-bonus');
-var jobsTagsHabilities = document.getElementById('registrationJobs-tags-habilities').children;
+var jobsActivities = document.getElementById('registrationJobs-description-activities');
+var jobsTagsHabilitiesHard = document.getElementById('registrationJobs-tags-habilities-hard').children;
+var jobsTagsHabilitiesSoft = document.getElementById('registrationJobs-tags-habilities-soft').children;
 var jobsTagsBonus = document.getElementById('registrationJobs-tags-bonus').children;
 var iconConfirm = document.getElementById('registrationJobs-icon-confirm');
 
-console.log(jobsTagsHabilities);
 
-function iteradorTagsHabilities() {
-	var arrHabilities = [];
-	for (item of jobsTagsHabilities) {
+jobsType.addEventListener('input', function () {
+	console.log(this.value)
+	if (this.value == "Estagio") {
+		jobsTime.disabled = true
+		jobsProficiency.disabled = true
+		jobsTime.style.backgroundColor = 'gray'
+		jobsProficiency.style.backgroundColor = 'gray'
+	} else {
+		jobsTime.disabled = false
+		jobsProficiency.disabled = false
+		jobsTime.style.backgroundColor = 'white'
+		jobsProficiency.style.backgroundColor = 'white'
+	}
+})
+
+jobsCP.addEventListener('input', function () {
+	if (isNaN(this.value)) {
+		this.value = this.value.slice(0,-1)
+	}
+})
+
+function iteradorTagsHabilitiesHard() {
+	var arrHabilitiesHard = [];
+	for (item of jobsTagsHabilitiesHard) {
 		if (item.className == 'registrationJobs-button-tag true') {
-			arrHabilities.push(item.value);
+			arrHabilitiesHard.push(item.value);
 		}
 	}
-	return arrHabilities;
+	return arrHabilitiesHard;
+}
+
+function iteradorTagsHabilitiesSoft() {
+	var arrHabilitiesSoft = [];
+	for (item of jobsTagsHabilitiesSoft) {
+		if (item.className == 'registrationJobs-button-tag true') {
+			arrHabilitiesSoft.push(item.value);
+		}
+	}
+	return arrHabilitiesSoft;
 }
 
 function iteradorTagsBonus() {
@@ -89,30 +119,51 @@ function iteradorTagsBonus() {
 
 var informationJob;
 
-iconConfirm.addEventListener('click', function (e) {
+
+var botaoDispara = new Promise(function (resolve, reject) { 
+	iconConfirm.addEventListener('click', function (e) {
 	e.preventDefault();
-	informationJob = {
-		job_name: jobsName.value,
+
+	resolve(informationJob = {
+		postal_code: parseInt(jobsCP.value),
+		company: pegaIdEmpresa(),
+		activities: jobsActivities.value,
+		name: jobsName.value,
+		description: jobsDescription.value,
 		type: jobsType.value,
-		time: jobsTime.value === 'full-time' ? 6 : 4,
-		salary: jobsSalary.value,
+		requireds_hardskill: iteradorTagsHabilitiesHard().join(','),
+		requireds_softskill: iteradorTagsHabilitiesSoft().join(','),
+		bonus: iteradorTagsBonus().join(','),
+		salary_min: parseFloat(jobsSalaryMin.value),
+		salary_max: parseFloat(jobsSalaryMax.value),
+		contact: pegaIdEmpresa(),
+		scholarship: jobsSchooling.value,
 		modality: jobsModality.value,
 		shift: jobsShift.value,
-		number: jobsNumber.value,
-		postal_code: jobsCP.value,
+		workload: jobsTime.value === 'full-time' ? 6 : 4,
 		proficiency: jobsProficiency.value,
-		name_company: jobsNameCompany.value,
-		job_description: jobsDescription.value,
-		description: companyDescription.value,
-		email: jobsEmail.value,
-		scholarship: jobsSchooling.value,
-	};
-	postarCadastro();
-});
+	});
+	reject('Não deu')
 
-function postarCadastro() {
-	$.post('http://127.0.0.1:3000/api/jobs/old', informationJob, function (response) {
+})}).then((res) => {
+	postarCadastro(res)
+}).catch((res) => {
+	console.log(res)
+})
+
+function postarCadastro(information) {
+	$.post('http://127.0.0.1:3000/api/jobs/', information, function (response) {
 		console.log(response);
 	});
-	alert('Cadastrado com sucesso');
 }
+
+
+function pegaIdEmpresa () {
+	var resposta
+	$.get('http://127.0.0.1:3000/api/companies/').done( function (response) {
+		resposta = response[response.length -1].id
+		console.log(resposta)
+		return resposta
+	})
+}
+
