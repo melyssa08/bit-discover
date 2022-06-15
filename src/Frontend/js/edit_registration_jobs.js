f59863c5f16c79e46746db091f8f4d7a5a2aa08e
MihaellK <MihaellK@users.noreply.github.com>
2022-06-15 03:33:21 -0300

    Pagina de edição de vagas e botão de delete
    -Falta o post e delete atraves da api_jobs
    -corrigido pequeno erro de gramatica em "proficiency" no registration_jobs

---
+++ src/Frontend/js/edit_registration_jobs.js
@@ -0,0 +1,60 @@
+// função pra ler querystring e pegar id do job para edição
+function queryString(parameter) {  
+	var loc = location.search.substring(1, location.search.length);   
+	var param_value = false;   
+	var params = loc.split("&");   
+	for (i=0; i<params.length;i++) {   
+		param_name = params[i].substring(0,params[i].indexOf('='));   
+		if (param_name == parameter) {                                          
+			param_value = params[i].substring(params[i].indexOf('=')+1)   
+		}   
+	}   
+	if (param_value) {   
+		return param_value;   
+	}   
+	else {   
+		return undefined;   
+	}   
+}
+
+//Var que recebe o id do job
+var idJob = queryString("idJob");
+// console.log("id do Job: "+idJob);
+
+
+
+
+$(document).ready(
+
+	$.get("http://localhost:3000/api/jobs", function (dados) {
+		
+		// var test = $('#registrationJobs-name-job').val();
+		// test = dados[idJob].name;
+
+		//Seta os valores do html para os valores do DB
+		$(jobsName).val(dados[idJob].name);
+		$(jobsType).val(dados[idJob].type);
+		$(jobsTime).val(dados[idJob].worload).change();
+		$(jobsModality).val(dados[idJob].modality).change();
+		$(jobsShift).val(dados[idJob].shift);
+		$(jobsSalaryMin).val(dados[idJob].salary_min);
+		$(jobsSchooling).val(dados[idJob].scholarship).change;
+		$(jobsSalaryMax).val(dados[idJob].salary_max);
+		$(jobsCP).val(dados[idJob].postal_code);
+		$(jobsProficiency).val(dados[idJob].proficiency);
+		$(jobsNameCompany).val(dados[idJob].companu);
+		$(jobsDescription).val(dados[idJob].description);
+		$(jobsActivities).val(dados[idJob].activities);
+		$(jobsTagsHabilitiesHard).val(dados[idJob].requireds_hardskill);
+		$(jobsTagsHabilitiesSoft).val(dados[idJob].requireds_softskill);
+		$(jobsTagsBonus).val(dados[idJob].bonus);
+
+
+
+	
+		console.log(dados[idJob]);
+		// console.log(jobsName);
+	})
+
+);
+
--- src/Frontend/page_recruiter/my_jobs/js/my_jobs.js
+++ src/Frontend/page_recruiter/my_jobs/js/my_jobs.js
@@ -10,55 +10,97 @@ window.addEventListener('load', function () {
 	});
 });
 
-//TESTE DE BANCO DE DADOS
-// api='http://127.0.0.1:3071'
+//  function teste() {
+// 	console.log("Deu certo!");
+// 	// $.get("http://localhost:3003/myjobsCard",);
+// 	// $("#answerQuestions").html('');
+// 	resultado = [
+// 		{
+// 			postal_code: 05510020,
+// 			name: 'Share Butantã',
+// 			email: 'sharebutanta@gmail.com',
+// 		},
+// 	];
 
-// function GetCardJob(){
-//     $.get(`http://127.0.0.1:3071/`, function(resultado) {
-//         var tx = ""
+// 	var count = 0;
+// 	console.log(resultado.length);
+// 	while (count < resultado.length) {
+// 		$('.myJobs-main').append('Nome: ', resultado[count].company_name, 'Email: ', resultado[count].email, ', CEP: "', resultado[count].postal_code, '" <br>');
+// 		count++;
+// 	}
+// 	// resultado.map((resul) => {
+// 	//     $("#nome").append(resul.name, resul.age);
+// 	// })
+//  }
 
-//         if(Array.isArray(resultado)){
+// function getJobOnLoad() {
+// 	$.get('http://localhost:3000/api/jobs', function (result) {
+// 		//  console.log("Resultado", result);
 
-//             resultado.forEach(element => {
-//                 tx += '<div class="myJobs-card">';
-//                 tx += '<div class="myJobs-card-title">' + element.name + '</div>';
-//                 tx += '<div class="myJobs-card-description">';
-//                 tx += '</div>';
-//                 tx += '</div>';
-//             });
+// 		if (result.length == 0) {
+// 			console.log('teste');
+// 			$('.myJobs-main').html(`<h2>Você atualmente não possui vagas registradas, registre uma vaga que ela aparecerá aqui!</h2>`);
+// 		} else {
+// 			for (i = 0; i < result.length; i++) {
+// 				$('.myJobs-main').append(`
+// 					<div class="myJobs-card" data-bs-toggle="modal" data-bs-target="#modalTeste${i}">
+// 						<h7 class="myJobs-card-title">${result[i].name}</h7>
+// 						<p>Modalidade: ${result[i].modality}<br>
+// 							Descrição: ${result[i].description}
+// 						</p>
+// 					</div>
+// 					<div class="modal fade" id="modalTeste${i}" tabindex="-1" role="dialog">
+// 						<div class="modal-dialog" role="document">
+// 							<div class="modal-content">
 
-//         } else {
-//             tx = resultado;
-//         }
-//         $("#myJobs-main").html(tx);
-//     })
+// 								<div class="modal-header">
+// 									<h5 class="modal-title">Modal Titulo</h5>
+// 									<button type="button" class="close" data-bs-dismiss="modal">
+// 										<span>&times;</span>
+// 									</button>
+// 								</div>
+
+// 								<div class="modal-body">
+// 									<p>${result[i].description}</p>
+// 								</div>
+
+// 								<div class="modal-footer">
+// 									<a class="btn btn-light" href="#" role="button" onclick="teste()">Editar Vaga</a>
+// 									<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Deletar Vaga</button>
+// 								</div>
+
+// 							</div>
+// 						</div>
+// 					</div>
+//     			`);
+// 			}
+// 		}
+// 	});
 // }
 
-function teste() {
-	// $.get("http://localhost:3003/myjobsCard",);
-	// $("#answerQuestions").html('');
-	resultado = [
-		{
-			postal_code: 05510020,
-			name: 'Share Butantã',
-			email: 'sharebutanta@gmail.com',
-		},
-	];
 
-	var count = 0;
-	console.log(resultado.length);
-	while (count < resultado.length) {
-		$('.myJobs-main').append('Nome: ', resultado[count].company_name, 'Email: ', resultado[count].email, ', CEP: "', resultado[count].postal_code, '" <br>');
-		count++;
-	}
-	// resultado.map((resul) => {
-	//     $("#nome").append(resul.name, resul.age);
-	// })
+//TESTE DE FUNÇOES EDIT E DELETE VAGA
+
+function editJob(id) {
+
+	console.log(id);
+
+	window.location.href = "http://localhost:3000/page_recruiter/edit_registration_jobs/index.html?idJob="+id;
+
 }
+
+function deleteJob(id){
+
+	$("#card"+id).remove();
+	//Pedir confirmação antes de relamente deletar
+	//implementar o api_jobs.delete!!
+
+}
+
 
 function getJobOnLoad() {
 	$.get('http://localhost:3000/api/jobs', function (result) {
-		// console.log("Resultado", result);
+		//  console.log("Resultado", result);
 
 		if (result.length == 0) {
 			console.log('teste');
@@ -66,40 +108,42 @@ function getJobOnLoad() {
 		} else {
 			for (i = 0; i < result.length; i++) {
 				$('.myJobs-main').append(`
-        <div class="myJobs-card" data-bs-toggle="modal" data-bs-target="#modalTeste${i}">
-        <h7 class="myJobs-card-title">${result[i].name}</h7>
-        <p>Modalidade: ${result[i].modality}<br>
-        Descrição: ${result[i].description}
-        </p>
-    </div>
-    <div class="modal fade" id="modalTeste${i}" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
+					<div class="myJobs-card" data-bs-toggle="modal" data-bs-target="#modalTeste${i}" id="card${i}">
+						<h7 class="myJobs-card-title">${result[i].name}</h7>
+						<p>Modalidade: ${result[i].modality}<br>
+							Descrição: ${result[i].description}
+						</p>
+					</div>
+					<div class="modal fade" id="modalTeste${i}" tabindex="-1" role="dialog">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
 
-            <div class="modal-header">
-                <h5 class="modal-title">Modal Titulo</h5>
-                <button type="button" class="close" data-bs-dismiss="modal">
-                    <span>&times;</span>
-                </button>
-            </div>
+								<div class="modal-header">
+									<h5 class="modal-title">Modal Titulo</h5>
+									<button type="button" class="close" data-bs-dismiss="modal">
+										<span>&times;</span>
+									</button>
+								</div>
 
-            <div class="modal-body">
-                <p>${result[i].description}
-                </p>
-            </div>
+								<div class="modal-body">
+									<p>${result[i].description}</p>
+								</div>
 
-            <div class="modal-footer">
-                <a class="btn btn-light" href="#" role="button">Editar Vaga</a>
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Deletar Vaga</button>
-            </div>
+								<div class="modal-footer">
+									<a class="btn btn-light" href="#" role="button" onclick="editJob(${result[i].id})">Editar Vaga</a>
+									<button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="deleteJob(${result[i].id})">Deletar Vaga</button>
+								</div>
 
-        </div>
-    </div>
-</div>
-    `);
+							</div>
+						</div>
+					</div>
+    			`);
 			}
 		}
 	});
 }
+
+
+
 
 $(document).ready;
