8f650bf949747887e005e018dc83ffa20dc1a558
gabreurt <gabriel.torres@sou.inteli.edu.br>
2022-06-18 14:49:41 -0300

    Skills especificas puxadas do banco de dados!

--- src/Frontend/js/registrationCandidate-js.js
+++ src/Frontend/js/registrationCandidate-js.js
@@ -19,24 +19,55 @@
 //     }
 //   });
 // }
-function botao() {
-  this.setAttribute("class","myProfile-button-tag-clicked")
-}
+var softSkills = [];
+var hardSkills = [];
 //executa a função ao iniciar a pagina adicionando os botões de hard e softskills
 function onload() {
-   //carrega os botões de softskill e os adiciona na página
-  $.get("http://localhost:3000/api/softskills", function(softskills) {
+  //carrega os botões de softskill e os adiciona na página
+  $.get("http://localhost:3000/api/softskills", function (softskills) {
     console.log(softskills)
-    for (i=0;i<softskills.length;i++) {
-      $('#registrationCandidates-content-obligation').append(`<button class="registrationCandidates-button-tag-unclicked" onclick="botao" id="s${softskills[i].id}">` + softskills[i].name + `</button>`)
-    }
-  }) 
-
-  
-  $.get('http://localhost:3000/api/hardskills', function(hardskills) {
-        //carrega os botões de hardskill e os adiciona na página
-    for (i=0;i<hardskills.length;i++) {
-      $('#registrationCandidates-content-obligation-1').append(`<button class="registrationCandidates-button-tag-unclicked" onclick="botao" id="h${hardskills[i].id}">` + hardskills[i].name + `</button>`)
+    for (i = 0; i < softskills.length; i++) {
+      $('#registrationCandidates-content-obligation').append(`<button class="registrationCandidates-button-tag-unclicked" onclick="selectSoftSkill(${softskills[i].id})" id="s${softskills[i].id}">` + softskills[i].name + `</button>`)
     }
   })
-   }
+
+
+  $.get('http://localhost:3000/api/hardskills', function (hardskills) {
+    //carrega os botões de hardskill e os adiciona na página
+    for (i = 0; i < hardskills.length; i++) {
+      $('#registrationCandidates-content-obligation-1').append(`<button class="registrationCandidates-button-tag-unclicked" onclick="selectHardSkill(${hardskills[i].id})" id="h${hardskills[i].id}">` + hardskills[i].name + `</button>`)
+    }
+  })
+}
+
+function selectSoftSkill(id) {
+    //busca se o id já exise no array
+    if (softSkills.indexOf(id)>=0) {
+      //se existe, ele romeve do array o id
+      softSkills.splice(softSkills.indexOf(id), 1);
+      //e retira a classe de clicado
+      document.getElementById("s"+id).classList.remove("registrationCandidates-button-tag-clicked");
+    }else{
+      //se não existe, adiciona ao array
+      softSkills.push(id);
+      //e adiciona a classe clicada
+      document.getElementById("s"+id).classList.add("registrationCandidates-button-tag-clicked");
+    }
+  
+}
+
+function selectHardSkill(id) {
+  //busca se o id já exise no array
+  if (hardSkills.indexOf(id)>=0) {
+    //se existe, ele romeve do array o id
+    hardSkills.splice(hardSkills.indexOf(id), 1);
+    //e retira a classe de clicado
+    document.getElementById("h"+id).classList.remove("registrationCandidates-button-tag-clicked");
+  }else{
+    //se não existe, adiciona ao array
+    hardSkills.push(id);
+    //e adiciona a classe clicada
+    document.getElementById("h"+id).classList.add("registrationCandidates-button-tag-clicked");
+  }
+
+}
