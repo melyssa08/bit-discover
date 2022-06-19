ce50bafb949b1e881b03a4cef2b7958164c73a1b
angrysine <99188421+angrysine@users.noreply.github.com>
2022-06-19 15:04:46 -0300

    chamadas da api e consertos gerais
    chamadas da api nas paginas e mudanças e cadastro dos perfis da empresa e da candiata

--- src/Frontend/js/myProfile-js.js
+++ src/Frontend/js/myProfile-js.js
@@ -7,32 +7,39 @@ function botao(id) {
     id.setAttribute("class","myProfile-button-tag-unclicked")
   }
 }
+//o butão que deixa editar os campos
 function pencilbutton() {
   var inputs = document.querySelectorAll("input")
   var selects = document.querySelectorAll("select")
   var buttons = document.querySelectorAll(".myProfile-button-tag")
   var textarea = document.querySelectorAll("textarea")
+  //após apertar o botão permite que os campos sejam modificados
   for (let i = 0; i < inputs.length; i++) {
     inputs[i].disabled = false
     inputs[i].classList.remove("myProfile-disabled")
   }
+  //após apertar o botão permite que os campos sejam modificados
   for (let g = 0; g < selects.length; g++) {
     selects[g].disabled = false
     selects[g].classList.remove("myProfile-disabled")
   }
+  //após apertar o botão permite que os campos sejam modificados
   for (let j = 0; j < textarea.length; j++) {
     textarea[j].disabled = false
     textarea[j].classList.remove("myProfile-disabled")
   }
+  //após apertar o botão permite que os campos sejam modificados
   for (let h = 0; h < buttons.length; h++) {
     buttons[h].disabled = false
   }
 }
-gn 
+
+//preenche os campos com os dados da usuária
 $.ajax({
   url: "http://127.0.0.1:3000/api/candidates/?id=1",
   type: "GET",
   success: function (data) {
+    console.log(data)
     $("#myProfile-input-name").val(data[0]["name"])
     $("#myProfile-input-email").val(data[0]["email"])
     $("#myProfile-input-confirm-email").val(data[0]["email"])
@@ -48,17 +55,21 @@ $.ajax({
 
   }
 })
+// atualiza os dados da usuária e vê se os campos esão preechidos corretamente
 function update_profile() {
+  // os if abaixo garantem que os dados estejam sendo inseridos de froma correta no formulário
+  if ( $("#myProfile-input-name").val().length ==0 || $("#myProfile-input-email").val().length == 0 || $("#myProfile-input-confirm-email").val().length ==0 || $("#myProfile-input-age").val().length ==0 || $("#myProfile-input-CPF").val().length == 0|| $("#myProfile-input-password").val().length == 0 ||  $("#myProfile-input-confirm-password").val().length == 0 ||$("#myProfile-input-cp").val().length == 0 ||$("#myProfile-textarea").val().length ==0){
+    alert("não deixe nenhum campo em branco")
+    return false
+  }
+
   if (!($("#myProfile-input-email").val() == $("#myProfile-input-confirm-email").val())) {
     return alert("Os emails não coincidem!")
   }
   if (!($("#myProfile-input-password").val() == $("#myProfile-input-confirm-password").val())) {
     return alert("As senhas não coincidem!")
   }
-
-  $.ajax({
-
-  })
+  //salva as mudanças no perfil do usário
   $.ajax({
     url: "http://127.0.0.1:3000/api/candidates/1",
     type: "PUT",
@@ -87,3 +98,39 @@ function onload() {
     }
   })
    }
+
+   
+var softSkills = [];
+var hardSkills = [];
+
+// deixar essas var no global
+function selectSoftSkill(id) {
+    //busca se o id já exise no array
+    if (softSkills.indexOf(id)>=0) {
+      //se existe, ele romeve do array o id
+      softSkills.splice(softSkills.indexOf(id), 1);
+      //e retira a classe de clicado
+      document.getElementById("s"+id).classList.remove("registrationCandidates-button-tag-clicked");
+    }else{
+      //se não existe, adiciona ao array
+      softSkills.push(id);
+      //e adiciona a classe clicada
+      document.getElementById("s"+id).classList.add("registrationCandidates-button-tag-clicked");
+    }
+
+};
+
+function selectHardSkill(id){
+  //busca se o id já exise no array
+  if (hardSkills.indexOf(id)>=0) {
+    //se existe, ele romeve do array o id
+    hardSkills.splice(hardSkills.indexOf(id), 1);
+    //e retira a classe de clicado
+    document.getElementById("h"+id).classList.remove("registrationCandidates-button-tag-clicked");
+  }else{
+    //se não existe, adiciona ao array
+    hardSkills.push(id);
+    //e adiciona a classe clicada
+    document.getElementById("h"+id).classList.add("registrationCandidates-button-tag-clicked");
+  }
+};
