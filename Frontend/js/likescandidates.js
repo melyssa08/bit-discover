587d93308593e735bdf16a8f6a43d1d52c5d2045
melyssa08 <melyssa.rojas@sou.inteli.edu.br>
2022-06-23 10:02:39 -0300

    Mudanças no front end do jobs visualization

--- Frontend/js/likescandidates.js
+++ Frontend/js/likescandidates.js
@@ -71,9 +71,7 @@ function loadCard(query = {}) {
 	let textao = Object.keys(query).map(key => key +"=" +query[key]).join("&")
 	let url = "/api/jobs?" + textao
   $.get("/api/candidates/" + userInfo.id, (res) => {
-    if (res[0]["likes"]) {
 			likesArray = res[0]["likes"].split(",")
-    }
 
 			console.log(likesArray)
       
